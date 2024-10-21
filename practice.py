630a563fe4b939f26d9398e231492902325310a7
Mohammed Kaashif Ameen <143896473+Kash4511@users.noreply.github.com>
2024-10-21 15:33:55 +0530

    practice.py

--- practice.py
+++ practice.py
@@ -4,19 +4,11 @@ print("-----------------------")
 print("Welcome to Hangman game")
 print("-----------------------")
 
-print("Easy Or Hard")
-x= input("Enter your choice: ")
 
-if x == "Easy":
-    words = ('apple', 'phone', 'python','car','lamp'"banana", "grape", "cherry")
-    chosen = random.choice(words)
-    display = ["_" for _ in chosen]
-    attempts = 8
-else:
-    words = ("elephant", "chocolate", "butterfly", "adventure", "mountain","television", "excellent", "sunflower")
-    chosen = random.choice(words)
-    display = ["_" for _ in chosen]
-    attempts = 16
+words = ("elephant", "chocolate", "butterfly", "adventure", "mountain","television", "excellent", "sunflower")
+chosen = random.choice(words)
+display = ["_" for _ in chosen]
+attempts = 16
 
 while attempts > 0 and "_" in display:
     print("\n" + ' '.join(display))  
