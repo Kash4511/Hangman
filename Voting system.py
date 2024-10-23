b2e191981c13050dcb742a6a7431b27b5b9a52d3
Mohammed Kaashif Ameen <143896473+Kash4511@users.noreply.github.com>
2024-10-23 20:56:41 +0530

    Voting system.py

--- Voting system.py
+++ Voting system.py
@@ -1,12 +1,15 @@
-def voter_ID():
+def voter_ID(used_id):
     result = []
     
     while True:
         id = input("Enter your Voter ID: ")
-        
+        if id in used_id:
+            print("The ID entered has already been used.")
+            return None
         user = input("Enter your Name: ")
         if "ID" in id and len(id) < 7:
             result.append("----------------------- \nUser Name: {}\nUser Voter ID: {}\n -----------------------".format(user, id))
+            used_id.append(id)
             
             break
         else:
@@ -14,21 +17,26 @@ def voter_ID():
             break
     
     return result
-voter_data = voter_ID()
-if voter_data:
-    for data in voter_data:
-        print(data)
-    vote = {'Alex': 0, 'Bob': 0, 'Tom': 0, 'Harry': 0}
-    print("The Candidates are:\n .Alex\n .Bob\n .Tom\n .Harry")
-    pick_candi = input("Enter the candidate you want to vote for: ") 
-    if pick_candi in vote:
-        vote[pick_candi] += 1
-        print("Vote casted for {}".format(vote))
+def candidate():
+    used_id = []
+    while True:
+        voter_data = voter_ID(used_id)
+        if voter_data:
+            for data in voter_data:
+                print(data)
+            vote = {'Alex': 0, 'Bob': 0, 'Tom': 0, 'Harry': 0}
+            print("The Candidates are:\n .Alex\n .Bob\n .Tom\n .Harry")
+            pick_candi = input("Enter the candidate you want to vote for: ") 
+            if pick_candi in vote:
+                vote[pick_candi] += 1
+                print("Vote casted for {}".format(vote))
     
-    else:
-        print("The entered candidate does not exist.")
+            else:
+                print("The entered candidate does not exist.")
     
-else:
-    print("No valid voter ID entered. Exiting the voting process.")
-
+        else:
+            print("No valid voter ID entered. Exiting the voting process.")
+            break
+    
+candidate()
 
